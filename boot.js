4edfb0c3625ce2db04f73ad8751909c62770342d
Angel Ricardo Elvira Torres <80663242+springi790@users.noreply.github.com>
2026-08-16 13:06:37 -0600

    Version Etik branded web app assets

--- boot.js
+++ boot.js
@@ -19,15 +19,15 @@
     try{
       const style=document.createElement('link');
       style.rel='stylesheet';
-      style.href='styles.css?v=15';
+      style.href='styles.css?v=16';
       document.head.appendChild(style);
 
-      await loadScript('qr-engine.js?v=15');
-      await loadScript('app.js?v=15');
-      await loadScript('enhancements.js?v=15');
-      await loadScript('menu.js?v=15');
-      await loadScript('accordion.js?v=15');
-      await loadScript('credits.js?v=15');
+      await loadScript('qr-engine.js?v=16');
+      await loadScript('app.js?v=16');
+      await loadScript('enhancements.js?v=16');
+      await loadScript('menu.js?v=16');
+      await loadScript('accordion.js?v=16');
+      await loadScript('credits.js?v=16');
 
       delete window.__etikCssGzip;
       delete window.__etikQrGzip;
